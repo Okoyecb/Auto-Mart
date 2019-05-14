a010b25fb6070779f77b86036e3483d94031a3df
Chidi Okoye <chidizy4@yahoo.com>
2019-05-14 19:00:04 +0100

    [feature #165862143] User can update price of ad posted

--- js/modal.js
+++ js/modal.js
@@ -1,25 +1,58 @@
 // Get the modal
 const modal = document.getElementById('myModal');
 
-// Get the button that opens the modal
-const btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-const span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
+btn = () =>{
   modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+closeDel = () =>{
   modal.style.display = "none";
 }
 
+
+
+
+
+// EDIT MODAL 1
+
+// Get the modal
+let edit = document.getElementById('editModal');
+
+// Get the <span> element that closes the modal
+let spanclosure = document.getElementsByClassName("closee")[0];
+
+// When the user clicks the button, open the modal 
+fire = () =>{
+  edit.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+closeModal = (edit) => {
+  let modal = document.getElementById(edit);
+  modal.style.display = "none";
+}
+
+
+// EDIT MODAL 2
+
+// Get the modal
+let trigger = document.getElementById('poModal');
+
+pull = () =>{
+  trigger.style.display = "block";
+}
+
+
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
+  if (event.target == edit) {
+    edit.style.display = "none";
+  }
   if (event.target == modal) {
     modal.style.display = "none";
+  }
+  if (event.target==trigger){
+    trigger.style.display="none";
   }
 }
