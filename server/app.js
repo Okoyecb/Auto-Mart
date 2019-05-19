b34c95b3907ab880166de5855ca13324706a9e3d
Chidi Okoye <chidizy4@yahoo.com>
2019-05-19 12:57:24 +0100

    [chore #166106932] Server setup completed

---
+++ server/app.js
@@ -0,0 +1,28 @@
+import express from 'express';
+
+const app = express();
+
+app.use(
+  express.urlencoded({
+    extended: false,
+  }),
+);
+app.use(express.json());
+
+app.use('/', (req, res) => {
+  res.status(200).json({
+    message: 'success',
+  });
+});
+
+// send 404 error to error handler
+app.use((req, res, next) => {
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log('Welcome to Auto-Mart'));
+
+export default app;
